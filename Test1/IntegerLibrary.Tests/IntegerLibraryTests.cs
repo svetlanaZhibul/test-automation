d814ac3d5ac97d293fb5c0d4b17d315fff4f153f
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-09-21 21:32:42 +0300

    Add C# first lab

---
+++ Test1/IntegerLibrary.Tests/IntegerLibraryTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntegerLibrary; //(1)
+using static IntegerLibrary.IntegerExtention; //only start with C# 6.0
+
+namespace IntegerLibrary.Tests
+{
+    [TestClass]
+    public class IntegerLibraryTests
+    {
+        [TestMethod]//атрибуты
+        public void IsSimpleNumber_11_isSimple_true()
+        {
+            //Arrange
+            int number = 11;
+            bool expected = true;
+            //Act
+            //IntegerExtention.IsSimpleNumber(number); (1)
+            bool actual = IsSimpleNumber(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        [TestMethod]//атрибуты
+        public void IsSimpleNumber_30_isSimple_false()
+        {
+            //Arrange
+            int number = 30;
+            bool expected = false;
+            //Act
+            //IntegerExtention.IsSimpleNumber(number); (1)
+            bool actual = IsSimpleNumber(number);
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+        [TestMethod]//атрибуты
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void IsSimpleNumber_Minus30_isSimple_Throw_OutOfRangeExeption()
+        {
+            int number = -30;
+            bool actual = IsSimpleNumber(number);
+
+        }
+    }
+}
---
+++ Test1/IntegerLibrary/IntegerExtention.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//unit тестирование
+namespace IntegerLibrary
+{
+    public class IntegerExtention
+    {
+        public static bool IsSimpleNumber(int number)
+        {
+            if (number<=1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+            for (int i=2; i<=number/2; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
