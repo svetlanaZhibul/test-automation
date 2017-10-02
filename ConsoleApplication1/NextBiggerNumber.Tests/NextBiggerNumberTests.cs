fb19152483ad3c34737934d3053d0b2f7a1e9aca
Rita Rusetskaya <r.rusetskaya@gmail.com>
2017-10-02 19:25:12 +0300

    Add project from C# course

---
+++ ConsoleApplication1/NextBiggerNumber.Tests/NextBiggerNumberTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace NextBiggerNumber.Tests
+{
+    [TestClass]
+    public class NextBiggerNumberTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestMethod1()
+        {
+            long number = -20;
+            long actual = Program.NextBiggerNumber(number);
+        }
+    }
+}
